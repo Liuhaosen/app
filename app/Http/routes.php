82cef7b878e29e57737a2262ecd809654124f7b4
Liuhaosen <919801928@qq.com>
2016-05-15 14:29:56 +0800

    init version

---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//后台首页的路由规则
+//路由是转发请求用的
+Route:;get('/admin','');
+
+
---
+++ liuhcheng.php
@@ -0,0 +1,7 @@
+1.使用composer命令来安装laravel框架
+	composer create-project laravel/laravel your-project-name --prefer-dist "5.1.*"
+
+2. 配置虚拟主机
+	如果需要检测域名解析是否成功,在cmd中直接ping这个域名
+
+3. 
